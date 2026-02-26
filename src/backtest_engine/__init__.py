5453993619ae2b41f458c12163e3f93b675b5f68
Arnaud BARBIER <arnaudbar50@gmail.com>
2026-02-26 19:38:14 +0100

    Update __init__.py

--- src/backtest_engine/__init__.py
+++ src/backtest_engine/__init__.py
@@ -1,1 +1,6 @@
+from .config import BacktestConfig
+from .pipeline import DataPipeline
+from .signals import Strategy_Signal
+from .engine import BacktestEngine
 
+__all__ = ["BacktestConfig", "DataPipeline", "Strategy_Signal", "BacktestEngine"]
