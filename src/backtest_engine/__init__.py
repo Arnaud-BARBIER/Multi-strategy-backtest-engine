4ab410181ce1434ec0d1ddcba347424fd4e51f49
Arnaud BARBIER <arnaudbar50@gmail.com>
2026-03-10 19:47:37 +0100

    Update __init__.py

--- src/backtest_engine/__init__.py
+++ src/backtest_engine/__init__.py
@@ -1,18 +1,9 @@
 from .config import BacktestConfig
-from .engine import NJITEngine, compute_metrics_full
-from .signals import (
-    ema_njit,
-    atr_wilder_njit,
-    signals_ema_vs_close_njit,
-    signals_ema_cross_njit,
-)
+from .engine import NJITEngine
+from .signals import signals_ema
 
 __all__ = [
     "BacktestConfig",
     "NJITEngine",
-    "compute_metrics_full",
-    "ema_njit",
-    "atr_wilder_njit",
-    "signals_ema_vs_close_njit",
-    "signals_ema_cross_njit",
+    "signals_ema",
 ]
