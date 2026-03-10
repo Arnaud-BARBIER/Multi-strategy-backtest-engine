4bc91285d17a398bc170153271c3e113756e4a08
Arnaud BARBIER <arnaudbar50@gmail.com>
2026-03-10 20:21:10 +0100

    Update __init__.py

--- src/backtest_engine/__init__.py
+++ src/backtest_engine/__init__.py
@@ -1,10 +1,9 @@
 from .config import BacktestConfig
 from .engine import NJITEngine
-from .signals import signals_ema
 
 __all__ = [
     "BacktestConfig",
     "NJITEngine",
-    "signals_ema",
 ]
-__version__ = "4.0.0"
+
+__version__ = "0.1.0"
