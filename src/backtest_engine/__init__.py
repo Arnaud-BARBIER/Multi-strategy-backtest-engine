a5e0180fe650e02329d0bb1e5809d0ce757e19d9
Arnaud BARBIER <arnaudbar50@gmail.com>
2026-03-10 21:31:09 +0100

    Update __init__.py

--- src/backtest_engine/__init__.py
+++ src/backtest_engine/__init__.py
@@ -1,9 +1,11 @@
 from .config import BacktestConfig
 from .engine import NJITEngine
+from .pipeline import DataPipeline
 
 __all__ = [
     "BacktestConfig",
     "NJITEngine",
+    "DataPipeline",
 ]
 
 __version__ = "0.1.0"
